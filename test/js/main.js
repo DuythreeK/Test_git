e139a154617d451671937cf64d1edda609bbe978
Nguyen Nhu Duy <nguyennhuduy865@gmail.com>
2026-08-17 18:02:07 +0700

    update

--- test/js/main.js
+++ test/js/main.js
@@ -3,4 +3,20 @@ import { products } from "./products.js";
 
 const productList = document.querySelector("#product-list");
 
-products.forEach(product)
+products.forEach((product, index) => {
+    if (index % 3 == 0) {
+        productList.innerHTML += `<tr id="row-${index}"></tr>`;
+    }
+    const row = document.querySelector(`"#row-${index - index % 3}"`);
+    row.innerHTML += `
+    <td>
+        <img src="${product.image}">
+        <h3>
+            ${product.name}
+        </h3>
+        <p>${product.chip}</p>
+        <p>${product.storage}<p>
+        <p><b>${product.price}</b></p>
+    </td>
+    `;
+})
