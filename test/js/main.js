d16b20368a0fbb7c6ca77064771f345cb35a2c15
Nguyen Nhu Duy <nguyennhuduy865@gmail.com>
2026-08-21 17:35:06 +0700

    update

--- test/js/main.js
+++ test/js/main.js
@@ -3,23 +3,25 @@ import { categories } from "./products.js";
 
 const productList = document.querySelector("#product-list");
 
-products.forEach((product, index) => {
-    if (index % 3 == 0) {
-        productList.innerHTML += `<tr id="row-${index}"></tr>`;
-    }
-    const row = document.querySelector(`#row-${index - index % 3}`);
-    row.innerHTML += `
-    <td>
-        <img src="${product.image}">
-        <h3>
-            ${product.name}
-        </h3>
-        <p>${product.chip}</p>
-        <p>${product.storage}<p>
-        <p><b>${product.price}</b></p>
-    </td>
-    `;
-})
+// products.forEach((product, index) => {
+//     if (index % 3 == 0) {
+//         productList.innerHTML += `<tr id="row-${index}"></tr>`;
+//     }
+//     const row = document.querySelector(`#row-${index - index % 3}`);
+//     row.innerHTML += `
+//     <td>
+//         <img src="${product.image}">
+//         <h3>
+//             ${product.name}
+//         </h3>
+//         <p>${product.chip}</p>
+//         <p>${product.storage}<p>
+//         <p><b>${product.price}</b></p>
+//     </td>
+//     `;
+// })
+
+dislayProducts(products);
 
 const productPriceTable = document.querySelector(("#product-price-table"));
 productPriceTable.innerHTML += `<tr>
@@ -45,3 +47,35 @@ categories.forEach((category) => {
     <li>${category}</li>
     `
 })
+
+const search = document.querySelector("#search");
+search.addEventListener("input", () => {
+    const searched = products.filter((product) => {
+        return product.name.toLowerCase().includes(search.value);
+    })
+    productList.innerHTML = "";
+    dislayProducts(searched);
+
+})
+
+const sortBy = document.querySelector('sort-by');
+sortBy.addEventListener('change', () => {
+    if (sortBy.value === "name-inc") dislayProducts(productList.sortBy)
+})
+function dislayProducts(products) {
+    products.forEach((product, index) => {
+        if (index % 3 == 0) productList.innerHTML += `<tr id="row-${index}"></tr>`;
+
+        const row = document.querySelector(`#row-${index - index % 3}`);
+        row.innerHTML += `
+        <td>
+            <image src="${product.image}"></image>
+            <h3>${product.name}</h3>
+            <p>${product.chip}</p>
+            <p>${product.chip}</p>
+            <p>${product.storage}</p>
+            <p><b>${product.price}</b></p>
+        </td>
+        `
+    })
+}
